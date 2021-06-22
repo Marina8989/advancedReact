9ed85dbac98603beed80943477e65e4f7859a6ab
Marina8989 <marina.mill209@gmail.com>
2021-06-22 11:41:58 -0700

    react useEffect

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,27 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [width, setWidth] = useState(window.innerWidth)
+
+  const checkSize = () => {
+    setWidth(window.innerWidth)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', checkSize);
+    console.log(width);
+    return () => {
+       window.removeEventListener('resize', checkSize);
+    }
+  }, [])
+
+
+  return (
+    <div>
+      <h2>Width of Window</h2>
+      <p>{width}</p>
+    </div>
+  )
 };
 
 export default UseEffectBasics;
